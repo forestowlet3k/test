a6c8d4c94ef0a26128a9a64685f686ef287b957d
forestowlet3k <53613759+forestowlet3k@users.noreply.github.com>
2020-08-23 01:00:34 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,2 +1,17 @@
-	    var testValue = 294;
-	    document.write("<p>JavaScript テスス<br>テストだよ</p>" + testValue);
+var testValue = 294;
+document.write("<p>JavaScript テスス<br>テストだよ</p>" + testValue);
+
+//csvローダー
+const outputElement = document.getElementById('output_csv');
+
+function getCsvData(dataPath) {
+ const request = new XMLHttpRequest();
+ request.addEventListener('load', (event) => {
+  const response = event.target.responseText;
+  outputElement.innerHTML = response;
+ });
+ request.open('GET', dataPath, true);
+ request.send();
+}
+
+getCsvData('test.csv');
