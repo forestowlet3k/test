6b7ab9604bdb95e40588f1aa4064e9bf2f66c102
forestowlet3k <53613759+forestowlet3k@users.noreply.github.com>
2020-08-23 02:19:29 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -8,7 +8,6 @@ function getCsvData(dataPath) {
  const request = new XMLHttpRequest();
  request.addEventListener('load', (event) => {
   const response = event.target.responseText;
-  console.log(response);
   outputElement.innerHTML = response;
  });
  request.open('GET', dataPath, true);
